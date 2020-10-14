812ea5e33ee4bebf867300c707b4e1eac2ecb284
Tyler Kindy <tylerkindy@gmail.com>
2020-10-13 22:51:57 -0400

    Clean up example routes

--- src/main/kotlin/com/tylerkindy/betrayal/server.kt
+++ src/main/kotlin/com/tylerkindy/betrayal/server.kt
@@ -1,13 +1,8 @@
 package com.tylerkindy.betrayal
 
 import com.zaxxer.hikari.HikariDataSource
-import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
-import io.ktor.http.ContentType
-import io.ktor.response.respond
-import io.ktor.response.respondText
-import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.serialization.DefaultJson
 import io.ktor.serialization.json
@@ -34,15 +29,6 @@ fun main() {
         }
 
         routing {
-            get("/") {
-                call.respondText("Hello, world!", ContentType.Text.Html)
-            }
-            get("random/{min}/{max}") {
-                val min = call.parameters["min"]?.toIntOrNull() ?: 0
-                val max = call.parameters["max"]?.toIntOrNull() ?: 10
-                val randomString = "${(min until max).shuffled().last()}"
-                call.respond(mapOf("value" to randomString))
-            }
             gameRoutes()
         }
     }.start(wait = true)
