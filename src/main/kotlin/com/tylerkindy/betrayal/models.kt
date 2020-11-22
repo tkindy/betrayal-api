fb6de4fe7a3f4c77f63f4332c91fc5a18573d5c4
Tyler Kindy <tylerkindy@gmail.com>
2020-11-22 09:09:51 -0500

    Add players to game

--- src/main/kotlin/com/tylerkindy/betrayal/models.kt
+++ src/main/kotlin/com/tylerkindy/betrayal/models.kt
@@ -2,8 +2,28 @@ package com.tylerkindy.betrayal
 
 import kotlinx.serialization.Serializable
 
+@Suppress("unused")
+@Serializable
+enum class CharacterColor {
+    RED, YELLOW, GREEN, BLUE, WHITE, PURPLE
+}
+
+@Serializable
+data class Trait(val value: Int, val index: Short)
+
+@Serializable
+data class Player(
+    val id: Int,
+    val characterName: String,
+    val color: CharacterColor,
+    val speed: Trait,
+    val might: Trait,
+    val sanity: Trait,
+    val knowledge: Trait
+)
+
 @Serializable
 data class GameRequest(val name: String)
 
 @Serializable
-data class Game(val id: String, val name: String)
+data class Game(val id: String, val name: String, val players: List<Player>)
--- src/main/kotlin/com/tylerkindy/betrayal/routes/games.kt
+++ src/main/kotlin/com/tylerkindy/betrayal/routes/games.kt
@@ -3,6 +3,10 @@ package com.tylerkindy.betrayal.routes
 import com.tylerkindy.betrayal.Game
 import com.tylerkindy.betrayal.GameRequest
 import com.tylerkindy.betrayal.Games
+import com.tylerkindy.betrayal.Player
+import com.tylerkindy.betrayal.Players
+import com.tylerkindy.betrayal.Trait
+import com.tylerkindy.betrayal.defs.characters
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
 import io.ktor.request.receiveOrNull
@@ -15,36 +19,45 @@ import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
-val gameIdCharacters = ('A'..'Z').toList()
-
 val gameRoutes: Routing.() -> Unit = {
     route("games") {
         get("{id}") {
             val id = call.parameters["id"]!!
-            val game = transaction {
+            val gameRow = transaction {
                 Games.select { Games.id eq id }
                     .firstOrNull()
-                    ?.let { Game(id = it[Games.id], name = it[Games.name]) }
-            }
+            } ?: return@get call.respond(HttpStatusCode.NotFound)
 
-            if (game == null) {
-                call.respond(HttpStatusCode.NotFound)
-                return@get
+            val players = transaction {
+                Players.select { Players.gameId eq id }
             }
+                .map {
+                    val characterId = it[Players.characterId]
+                    val characterDef = characters[characterId]
+                        ?: error("No character defined with ID $characterId")
 
-            call.respond(game)
+                    Player(
+                        id = it[Players.id],
+                        characterName = characterDef.name,
+                        color = characterDef.color,
+                        speed = buildTrait(it[Players.speedIndex], characterDef.speedScale),
+                        might = buildTrait(it[Players.mightIndex], characterDef.mightScale),
+                        sanity = buildTrait(it[Players.sanityIndex], characterDef.sanityScale),
+                        knowledge = buildTrait(it[Players.knowledgeIndex], characterDef.knowledgeScale)
+                    )
+                }
+
+            call.respond(
+                Game(id = gameRow[Games.id], name = gameRow[Games.name], players = players)
+            )
         }
 
         post {
             val gameRequest = call.receiveOrNull<GameRequest>()
+                ?: return@post call.respond(HttpStatusCode.BadRequest, "Missing 'name'")
 
-            if (gameRequest == null) {
-                call.respond(HttpStatusCode.BadRequest, "Missing 'name'")
-                return@post
-            }
             if (gameRequest.name.length > 32) {
-                call.respond(HttpStatusCode.BadRequest, "Name can only be up to 32 characters")
-                return@post
+                return@post call.respond(HttpStatusCode.BadRequest, "Name can only be up to 32 characters")
             }
 
             val gameId = buildGameId()
@@ -55,14 +68,18 @@ val gameRoutes: Routing.() -> Unit = {
                 }
             }
 
-            call.respond(Game(id = gameId, name = gameRequest.name))
+            call.respond(Game(id = gameId, name = gameRequest.name, players = emptyList()))
         }
     }
 }
 
-fun buildGameId(): String {
+private val gameIdCharacters = ('A'..'Z').toList()
+private fun buildGameId(): String {
     return (0 until 6)
         .map { gameIdCharacters.random() }
         .fold(StringBuilder()) { builder, char -> builder.append(char) }
         .toString()
 }
+
+private fun buildTrait(index: Short, scale: List<Int>): Trait =
+    Trait(value = scale[index.toInt()], index = index)
