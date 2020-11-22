fde27c8ce1a1c1007a4176459d8172d8f2d9cc26
Tyler Kindy <tylerkindy@gmail.com>
2020-11-21 22:10:20 -0500

    Add table definition

---
+++ src/main/kotlin/com/tylerkindy/betrayal/tables.kt
@@ -0,0 +1,22 @@
+package com.tylerkindy.betrayal
+
+import org.jetbrains.exposed.sql.Table
+
+object Games : Table() {
+    val id = varchar("id", 6)
+    val name = varchar("name", 32)
+
+    override val primaryKey = PrimaryKey(id)
+}
+
+object Players : Table() {
+    val id = integer("id")
+    val gameId = varchar("gameId", 6)
+    val characterId = short("characterId")
+    val speedIndex = short("speedIndex")
+    val mightIndex = short("mightIndex")
+    val sanityIndex = short("sanityIndex")
+    val knowledgeIndex = short("knowledgeIndex")
+
+    override val primaryKey = PrimaryKey(id)
+}
