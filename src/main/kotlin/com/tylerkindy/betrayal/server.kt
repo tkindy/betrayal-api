e247298ae992b0ea339d44147137af5ed95d9b43
Tyler Kindy <tylerkindy@gmail.com>
2020-11-25 12:35:06 -0500

    Add call logging

--- src/main/kotlin/com/tylerkindy/betrayal/server.kt
+++ src/main/kotlin/com/tylerkindy/betrayal/server.kt
@@ -3,6 +3,7 @@ package com.tylerkindy.betrayal
 import com.tylerkindy.betrayal.routes.gameRoutes
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.application.install
+import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.routing.routing
 import io.ktor.serialization.DefaultJson
@@ -11,6 +12,7 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.sql.Database
+import org.slf4j.event.Level
 
 fun main() {
     Database.connect(
@@ -27,6 +29,9 @@ fun main() {
                     prettyPrint = true
                 }
             )
+        }
+        install(CallLogging) {
+            level = Level.INFO
         }
 
         routing {
