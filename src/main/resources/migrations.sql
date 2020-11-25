19c28f2b1a22d8af6850fe530a6cf7c4fd4670ff
Tyler Kindy <tylerkindy@gmail.com>
2020-11-25 09:04:49 -0500

    Add rooms table migration

--- src/main/resources/migrations.sql
+++ src/main/resources/migrations.sql
@@ -20,3 +20,14 @@ CREATE TABLE "players" (
 );
 
 CREATE INDEX ON "players" ("gameId");
+
+--changeset tkindy:3
+CREATE TABLE "rooms" (
+    "id" SERIAL PRIMARY KEY,
+    "gameId" VARCHAR(6) NOT NULL,
+    "roomDefId" SMALLINT NOT NULL,
+    "gridX" INT NOT NULL,
+    "gridY" INT NOT NULL
+);
+
+CREATE INDEX ON "rooms" ("gameId");
