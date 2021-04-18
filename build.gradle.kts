b53c2e76e254f814954215bb780390539a2fc54c
Tyler Kindy <tylerkindy@gmail.com>
2021-04-18 19:48:06 -0400

    Upgrade Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.20"
+    kotlin("jvm") version "1.4.32"
     kotlin("plugin.serialization") version "1.4.20"
     application
     id("org.jmailen.kotlinter") version "3.3.0"
