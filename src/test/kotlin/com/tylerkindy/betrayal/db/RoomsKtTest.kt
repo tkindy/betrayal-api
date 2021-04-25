64ff2dbf6c3e164deff983971cb36959c6e0100f
Tyler Kindy <tylerkindy@gmail.com>
2021-04-25 13:23:56 -0400

    Switch to DescribeSpec

--- src/test/kotlin/com/tylerkindy/betrayal/db/RoomsKtTest.kt
+++ src/test/kotlin/com/tylerkindy/betrayal/db/RoomsKtTest.kt
@@ -2,7 +2,7 @@ package com.tylerkindy.betrayal.db
 
 import com.tylerkindy.betrayal.Direction
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.Exhaustive
@@ -23,36 +23,39 @@ val invalidRotations = arbitrary(listOf(Short.MIN_VALUE, -1, 4, Short.MAX_VALUE)
     it.random.nextInt(invalidRotationValues.indices).let { i -> invalidRotationValues[i] }.toShort()
 }
 
-class RoomsKtTest : StringSpec({
-    "rotateDoors maintains number of directions" {
-        forAll(directionSets, rotations) { dirs, rotation ->
-            rotateDoors(dirs, rotation).size == dirs.size
-        }
-    }
+class RoomsKtTest : DescribeSpec({
 
-    "rotateDoors throws for invalid rotations" {
-        checkAll(directionSets, invalidRotations) { dirs, rotation ->
-            shouldThrow<IllegalArgumentException> { rotateDoors(dirs, rotation) }
+    describe("rotateDoors") {
+        it("maintains number of directions") {
+            forAll(directionSets, rotations) { dirs, rotation ->
+                rotateDoors(dirs, rotation).size == dirs.size
+            }
         }
-    }
 
-    "rotateDoors returns the same directions with no rotation" {
-        forAll(directionSets) { dirs ->
-            rotateDoors(dirs, 0) == dirs
+        it("throws for invalid rotations") {
+            checkAll(directionSets, invalidRotations) { dirs, rotation ->
+                shouldThrow<IllegalArgumentException> { rotateDoors(dirs, rotation) }
+            }
         }
-    }
 
-    "rotateDoors always returns all directions" {
-        val allDirections = Direction.values().toSet()
-        forAll(rotations) { rotation ->
-            rotateDoors(allDirections, rotation) == allDirections
+        it("returns the same directions with no rotation") {
+            forAll(directionSets) { dirs ->
+                rotateDoors(dirs, 0) == dirs
+            }
         }
-    }
 
-    "rotateDoors rotates doors" {
-        rotateDoors(
-            setOf(Direction.NORTH, Direction.WEST),
-            3
-        ) shouldBe setOf(Direction.NORTH, Direction.EAST)
+        it("always returns all directions") {
+            val allDirections = Direction.values().toSet()
+            forAll(rotations) { rotation ->
+                rotateDoors(allDirections, rotation) == allDirections
+            }
+        }
+
+        it("rotates doors") {
+            rotateDoors(
+                setOf(Direction.NORTH, Direction.WEST),
+                3
+            ) shouldBe setOf(Direction.NORTH, Direction.EAST)
+        }
     }
 })
