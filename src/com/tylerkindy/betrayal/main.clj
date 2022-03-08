ef2611d836f98b19729089cccc9021b24424990a
Tyler Kindy <tylerkindy@gmail.com>
2022-03-08 17:20:13 -0500

    Simplify dev server helpers

--- src/com/tylerkindy/betrayal/main.clj
+++ src/com/tylerkindy/betrayal/main.clj
@@ -8,8 +8,6 @@
    :headers {"Content-Type" "text/html"}
    :body "Hello, World!"})
 
-(defonce server (atom nil))
-
 (def app
   (r/ring-handler
    (r/router
@@ -20,19 +18,15 @@
     (r/redirect-trailing-slash-handler {:method :strip})
     (r/create-default-handler))))
 
-(defn start-server! [join?]
-  (reset! server
-          (run-jetty app
-                     {:port 3000
-                      :join? join?})))
+(defn start-server [join?]
+  (run-jetty app {:port 3000
+                  :join? join?}))
 
-(defn stop-server! []
+(defonce server (atom nil))
+(defn reload! []
   (let [server @server]
-    (when server (.stop server))))
-
-(defn restart-server! []
-  (stop-server!)
-  (start-server! false))
+    (when server (.stop server)))
+  (reset! server (start-server false)))
 
 (defn -main []
-  (start-server! true))
+  (start-server true))
