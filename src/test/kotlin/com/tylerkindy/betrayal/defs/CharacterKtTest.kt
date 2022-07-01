1b465d640ea1605d61f1e2c8cea48e508606cce1
Tyler Kindy <tylerkindy@gmail.com>
2022-07-01 08:26:37 -0400

    Use DescribeSpec

--- src/test/kotlin/com/tylerkindy/betrayal/defs/CharacterKtTest.kt
+++ src/test/kotlin/com/tylerkindy/betrayal/defs/CharacterKtTest.kt
@@ -1,26 +1,28 @@
 package com.tylerkindy.betrayal.defs
 
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
-class CharacterKtTest : StringSpec({
-    "parseTraitDefinition works" {
-        parseTraitDefinition("2,2,3,5*,5,5,6,6") shouldBe TraitDefinition(
-            scale = listOf(2, 2, 3, 5, 5, 5, 6, 6),
-            startingIndex = 3
-        )
-    }
+class CharacterKtTest : DescribeSpec({
+    describe("parseTraitDefinition") {
+        it("works") {
+            parseTraitDefinition("2,2,3,5*,5,5,6,6") shouldBe TraitDefinition(
+                scale = listOf(2, 2, 3, 5, 5, 5, 6, 6),
+                startingIndex = 3
+            )
+        }
 
-    "parseTraitDefinition catches missing starting value" {
-        shouldThrow<IllegalArgumentException> { parseTraitDefinition("2,3,4,4,4,6,6") }
-    }
+        it("catches missing starting value") {
+            shouldThrow<IllegalArgumentException> { parseTraitDefinition("2,3,4,4,4,6,6") }
+        }
 
-    "parseTraitDefinition catches duplicate starting values" {
-        shouldThrow<IllegalArgumentException> { parseTraitDefinition("2,3*,4,4,4*,6,6") }
-    }
+        it("catches duplicate starting values") {
+            shouldThrow<IllegalArgumentException> { parseTraitDefinition("2,3*,4,4,4*,6,6") }
+        }
 
-    "parseTraitDefinition catches empty value" {
-        shouldThrow<IllegalArgumentException> { parseTraitDefinition("") }
+        it("catches empty value") {
+            shouldThrow<IllegalArgumentException> { parseTraitDefinition("") }
+        }
     }
 })
