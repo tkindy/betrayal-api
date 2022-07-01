477a9ad22682f08b34d1f8f6b50fdd5e3135ae82
Tyler Kindy <tylerkindy@gmail.com>
2022-07-01 08:33:19 -0400

    Add character parsing test

--- src/test/kotlin/com/tylerkindy/betrayal/defs/CharacterKtTest.kt
+++ src/test/kotlin/com/tylerkindy/betrayal/defs/CharacterKtTest.kt
@@ -1,10 +1,25 @@
 package com.tylerkindy.betrayal.defs
 
+import com.tylerkindy.betrayal.CharacterColor
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class CharacterKtTest : DescribeSpec({
+    describe("characters") {
+        it("parses correctly") {
+            characters[2] shouldBe CharacterDefinition(
+                id = 2,
+                name = "Missy Dubourde",
+                color = CharacterColor.YELLOW,
+                speed = TraitDefinition(listOf(3, 4, 5, 6, 6, 6, 7, 7), 2),
+                might = TraitDefinition(listOf(2, 3, 3, 3, 4, 5, 6, 7), 3),
+                sanity = TraitDefinition(listOf(1, 2, 3, 4, 5, 5, 6, 7), 2),
+                knowledge = TraitDefinition(listOf(2, 3, 4, 4, 5, 6, 6, 6), 3)
+            )
+        }
+    }
+
     describe("parseTraitDefinition") {
         it("works") {
             parseTraitDefinition("2,2,3,5*,5,5,6,6") shouldBe TraitDefinition(
